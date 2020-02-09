80a25139b4d68fa0a0baad1ed3db5d6d543992f9
DashaPyr <56103490+DashaPyr@users.noreply.github.com>
2020-02-10 01:56:33 +0300

    Задание 2. Вычисление расстояния между точками с помощью метода

--- sandbox/src/main/java/dd/pyrkova/FirstLesson.java
+++ sandbox/src/main/java/dd/pyrkova/FirstLesson.java
@@ -2,16 +2,8 @@ package dd.pyrkova;
 
 public class FirstLesson {
   public static void main(String[] args) {
-    Point p1 = new Point();
-    Point p2 = new Point();
-    p1.i = 1;
-    p1.j = 1;
-    p2.i = 5;
-    p2.j = 4;
-    System.out.println("Расстояние между точками {" + p1.i + "; " + p1.j + "} и {" + p2.i + "; " + p2.j + "} = " + distance(p1, p2));
-  }
-
-  public static double distance(Point p1, Point p2){
-    return Math.sqrt((p2.i - p1.i) * (p2.i - p1.i) + (p2.j - p1.j) * (p2.j - p1.j));
+    Point p1 = new Point(1,1);
+    Point p2 = new Point(5,4);
+    System.out.println("Расстояние между точками {" + p1.i + "; " + p1.j + "} и {" + p2.i + "; " + p2.j + "} = " + p1.distance(p2));
   }
 }
