43d3a25878121922837500a72da845363a251677
DashaPyr <56103490+DashaPyr@users.noreply.github.com>
2020-02-09 19:50:13 +0300

    урок 1.02

--- sandbox/src/main/java/dd/pyrkova/MyFirstProgram.java
+++ sandbox/src/main/java/dd/pyrkova/MyFirstProgram.java
@@ -1,9 +1,8 @@
 package dd.pyrkova;
 
 public class MyFirstProgram {
-
-	public static void main(String[] args){
-		System.out.println("Hello, world!");
-	}
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
 
 }
