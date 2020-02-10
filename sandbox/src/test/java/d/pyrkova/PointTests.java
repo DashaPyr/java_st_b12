16f62319a94dd2f70875040264f132b3277dea79
DashaPyr <56103490+DashaPyr@users.noreply.github.com>
2020-02-11 00:23:09 +0300

    Задание 3. Проваленный тест

--- sandbox/src/test/java/d/pyrkova/PointTests.java
+++ sandbox/src/test/java/d/pyrkova/PointTests.java
@@ -7,8 +7,8 @@ public class PointTests {
 
   @Test
   public void testDistance (){
-    Point p1 = new Point(4,5);
-    Point p2 = new Point(8,8);
+    Point p1 = new Point(4,3);
+    Point p2 = new Point(8,9);
     Assert.assertEquals(p1.distance(p2),5);
   }
 }
