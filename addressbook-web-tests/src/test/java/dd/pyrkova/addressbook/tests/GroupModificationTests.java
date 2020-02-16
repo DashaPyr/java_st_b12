32d9cb50d92719fe5471e2a574ea1403f6cfc04e
DashaPyr <56103490+DashaPyr@users.noreply.github.com>
2020-02-16 20:02:23 +0300

    Урок 2.9 Создание теста на модификацию группы без рекордера

---
+++ addressbook-web-tests/src/test/java/dd/pyrkova/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,19 @@
+package dd.pyrkova.addressbook.tests;
+
+import dd.pyrkova.addressbook.model.GroupData;
+import dd.pyrkova.addressbook.model.TestBase;
+import org.testng.annotations.Test;
+
+public class GroupModificationTests extends TestBase {
+
+  @Test
+  public void testGroupModification() throws Exception {
+    app.getNavigationHelper().gotoGroupPage();
+    app.getSelectHelper().selectElement();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test11", "test22", "test33"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+    app.getSessionHelper().logout();
+  }
+}
