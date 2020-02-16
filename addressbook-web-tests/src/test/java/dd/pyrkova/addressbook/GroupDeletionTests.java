35e1e17f15151d7d1c4a137a528ef5d6d6677dbd
DashaPyr <56103490+DashaPyr@users.noreply.github.com>
2020-02-16 14:08:33 +0300

    Урок 2.5 Создание общего класса для тестов

---
+++ addressbook-web-tests/src/test/java/dd/pyrkova/addressbook/GroupDeletionTests.java
@@ -0,0 +1,15 @@
+package dd.pyrkova.addressbook;
+
+import org.testng.annotations.*;
+
+public class GroupDeletionTests extends TestBase{
+
+  @Test
+  public void testGroupDeletion() throws Exception {
+    gotoGroupPage();
+    selectGroup();
+    deleteSelectedGroups();
+    returnToGroupPage();
+  }
+
+}
