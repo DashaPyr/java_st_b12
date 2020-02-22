57dc7a55b4222bcb2e1fd704d14ebf8b1ab016a9
DashaPyr <56103490+DashaPyr@users.noreply.github.com>
2020-02-22 12:30:00 +0300

    Урок 3.3

--- addressbook-web-tests/src/test/java/dd/pyrkova/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/dd/pyrkova/addressbook/appmanager/ApplicationManager.java
@@ -2,6 +2,9 @@ package dd.pyrkova.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,10 +18,23 @@ public class ApplicationManager {
   private NavigationHelper navigationHelper;
   private UserHelper userHelper;
   private GroupHelper groupHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    System.setProperty("webdriver.chrome.driver", "c:\\Windows\\System32\\chromedriver.exe");
-    wd = new ChromeDriver();
+ //   String browser = BrowserType.CHROME;
+    if (browser == BrowserType.CHROME){
+      System.setProperty("webdriver.chrome.driver", "c:\\Windows\\System32\\chromedriver.exe");
+      wd = new ChromeDriver();
+    } else if (browser == BrowserType.FIREFOX){
+      wd = new FirefoxDriver();
+    } else if (browser == BrowserType.IE){
+      wd = new InternetExplorerDriver();
+    }
+
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/group.php");
     groupHelper = new GroupHelper(wd);
