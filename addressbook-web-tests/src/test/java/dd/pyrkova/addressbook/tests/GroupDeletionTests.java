b7942f5b070d5bd4609e82fc35dee658a59761f6
DashaPyr <56103490+DashaPyr@users.noreply.github.com>
2020-02-29 14:17:28 +0300

    Урок 4.5. Ч2 GroupModification GroupDeletion

--- addressbook-web-tests/src/test/java/dd/pyrkova/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/dd/pyrkova/addressbook/tests/GroupDeletionTests.java
@@ -5,6 +5,8 @@ import dd.pyrkova.addressbook.model.TestBase;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.util.List;
+
 public class GroupDeletionTests extends TestBase {
 
   @Test
@@ -13,12 +15,12 @@ public class GroupDeletionTests extends TestBase {
     if (! app.getGroupHelper().isThereAGroup()){
       app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
     }
-    int before = app.getGroupHelper().getGroupCount();
-    app.getGroupHelper().selectGroup(before - 1);
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before - 1);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() - 1);
   }
 
 }
