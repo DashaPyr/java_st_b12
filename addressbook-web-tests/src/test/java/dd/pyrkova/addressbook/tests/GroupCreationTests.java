77620e006366ed7cf2fad117aadb1595fe8ea3c3
DashaPyr <56103490+DashaPyr@users.noreply.github.com>
2020-02-29 14:06:56 +0300

    Урок 4.5 ч1 GroupCreationTests

--- addressbook-web-tests/src/test/java/dd/pyrkova/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/dd/pyrkova/addressbook/tests/GroupCreationTests.java
@@ -5,16 +5,19 @@ import dd.pyrkova.addressbook.model.TestBase;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.util.List;
+
 public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+//    int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
-//    app.getSessionHelper().logout();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before+1);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+ //   int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after.size(), before.size() + 1);
   }
 
 }
