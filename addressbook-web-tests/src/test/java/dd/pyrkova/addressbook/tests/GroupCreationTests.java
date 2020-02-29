309349708cb859ca2a9b757cbe04ebe9cc26fd46
DashaPyr <56103490+DashaPyr@users.noreply.github.com>
2020-02-29 21:04:47 +0300

    Урок 4.8 Максимальный элемент в коллекции

--- addressbook-web-tests/src/test/java/dd/pyrkova/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/dd/pyrkova/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,7 @@ import dd.pyrkova.addressbook.model.TestBase;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
@@ -14,10 +15,23 @@ public class GroupCreationTests extends TestBase {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
 //    int before = app.getGroupHelper().getGroupCount();
-    app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+    GroupData group = new GroupData("test1", "test2", "test3");
+    app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
  //   int after = app.getGroupHelper().getGroupCount();
     Assert.assertEquals(after.size(), before.size() + 1);
+
+
+    int max = 0;
+    for (GroupData g : after){
+      if (g.getId() > max){
+        max = g.getId();
+      }
+    }
+
+    group.setId(max);
+    before.add(group);
+    Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
   }
 
 }
