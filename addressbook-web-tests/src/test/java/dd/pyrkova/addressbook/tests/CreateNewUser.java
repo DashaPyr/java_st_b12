10129a233d4da199a556acaac5a2adbaf206d2d1
DashaPyr <56103490+DashaPyr@users.noreply.github.com>
2020-03-14 16:43:58 +0300

    Урок 6.4 Параметризация тестовых методов

--- addressbook-web-tests/src/test/java/dd/pyrkova/addressbook/tests/CreateNewUser.java
+++ addressbook-web-tests/src/test/java/dd/pyrkova/addressbook/tests/CreateNewUser.java
@@ -4,21 +4,43 @@ import dd.pyrkova.addressbook.model.*;
 import org.testng.annotations.*;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CreateNewUser extends TestBase {
 
-  @Test
-  public void testCreateNewUser() throws Exception {
-    app.goTo().homePage();
+  @DataProvider
+  public Iterator<Object[]> validUsers(){
+    List<Object[]> list = new ArrayList<Object[]>();
     File photo = new File("src/test/resouces/catbus.jpg");
-    Users before = app.user().allUser();
-    UserData user = new UserData().withFirstname("Daria").withMiddlename("Vladimirovna").withLastname("Pyrkova").withNickname("dd")
+    list.add(new Object[] {new UserData().withFirstname("Darya_11").withMiddlename("V_11").withLastname("Pyrkova_11").withNickname("dd")
             .withCompany("U").withAddress("Dolgoprudny").withEmailone("d@u.ru").withEmailtwo("d@g.com")
             .withPhonehome("999").withPhonemobile("777").withPhonework("888")
-            .withBirthday("1").withBirthmonth("January").withBirthyear("1990").withGroup("[none]").withPhoto(photo);
+            .withBirthday("1").withBirthmonth("January").withBirthyear("1990").withGroup("[none]").withPhoto(photo)});
+    list.add(new Object[] {new UserData().withFirstname("Darya_22").withMiddlename("V_22").withLastname("Pyrkova_22").withNickname("dd")
+            .withCompany("U").withAddress("Dolgoprudny").withEmailone("d@u.ru").withEmailtwo("d@g.com")
+            .withPhonehome("999").withPhonemobile("777").withPhonework("888")
+            .withBirthday("1").withBirthmonth("January").withBirthyear("1990").withGroup("[none]").withPhoto(photo)});
+    list.add(new Object[] {new UserData().withFirstname("Darya_21").withMiddlename("V_21").withLastname("Pyrkova_21").withNickname("dd")
+            .withCompany("U").withAddress("Dolgoprudny").withEmailone("d@u.ru").withEmailtwo("d@g.com")
+            .withPhonehome("999").withPhonemobile("777").withPhonework("888")
+            .withBirthday("1").withBirthmonth("January").withBirthyear("1990").withGroup("[none]").withPhoto(photo)});
+    return list.iterator();
+  }
+
+  @Test (dataProvider = "validUsers")
+  public void testCreateNewUser(UserData user) {
+    app.goTo().homePage();
+//    File photo = new File("src/test/resouces/catbus.jpg");
+    Users before = app.user().allUser();
+/*    UserData user = new UserData().withFirstname(firstname).withMiddlename(middlename).withLastname(lastname).withNickname("dd")
+            .withCompany("U").withAddress("Dolgoprudny").withEmailone("d@u.ru").withEmailtwo("d@g.com")
+            .withPhonehome("999").withPhonemobile("777").withPhonework("888")
+            .withBirthday("1").withBirthmonth("January").withBirthyear("1990").withGroup("[none]").withPhoto(photo); */
     app.user().create(user, true);
     app.goTo().homePage();
     assertThat(app.user().userCount(), equalTo(before.size() + 1));
--- addressbook-web-tests/src/test/java/dd/pyrkova/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/dd/pyrkova/addressbook/tests/GroupCreationTests.java
@@ -6,24 +6,36 @@ import dd.pyrkova.addressbook.model.TestBase;
 import org.testng.annotations.*;
 
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validGroups() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new GroupData().withName("test_1").withHeader("header_1").withFooter("footer_1")});
+    list.add(new Object[] {new GroupData().withName("test_2").withHeader("header_2").withFooter("footer_2")});
+    list.add(new Object[] {new GroupData().withName("test_3").withHeader("header_3").withFooter("footer_3")});
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) {
     app.goTo().groupPage();
     Groups before = app.group().allGroup();
-    GroupData group = new GroupData().withName("test35").withHeader("test2").withFooter("test3");
     app.group().create(group);
     Groups after = app.group().allGroup();
     assertThat(app.group().groupCount(), equalTo(before.size() + 1));
     assertThat(after, equalTo(
-            before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+          before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
 
-  @Test
+/*  @Test  (enabled = false)
   public void testBadGroupCreation() throws Exception {
     app.goTo().groupPage();
     Groups before = app.group().allGroup();
@@ -32,5 +44,5 @@ public class GroupCreationTests extends TestBase {
     assertThat(app.group().groupCount(), equalTo(before.size()));
     Groups after = app.group().allGroup();
     assertThat(after, equalTo(before));
-  }
+  }*/
 }
