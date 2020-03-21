7967ccdc127c3c41ac4ab3c42315477be7878e1a
DashaPyr <56103490+DashaPyr@users.noreply.github.com>
2020-03-21 10:38:19 +0300

    less 7.1 for users

--- addressbook-web-tests/src/test/java/dd/pyrkova/addressbook/tests/DbConnectionTest.java
+++ addressbook-web-tests/src/test/java/dd/pyrkova/addressbook/tests/DbConnectionTest.java
@@ -2,6 +2,8 @@ package dd.pyrkova.addressbook.tests;
 
 import dd.pyrkova.addressbook.model.GroupData;
 import dd.pyrkova.addressbook.model.Groups;
+import dd.pyrkova.addressbook.model.UserData;
+import dd.pyrkova.addressbook.model.Users;
 import org.testng.annotations.Test;
 
 import java.sql.*;
@@ -15,17 +17,25 @@ public class DbConnectionTest {
     try {
       conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=&serverTimezone=UTC");
       Statement st = conn.createStatement();
-      ResultSet rs = st.executeQuery("select l.group_id, l.group_name, l.group_header, l.group_footer from group_list l");
+ //     ResultSet rs = st.executeQuery("select l.group_id, l.group_name, l.group_header, l.group_footer  from group_list l");
+      ResultSet us = st.executeQuery("select u.id, u.firstname, u.lastname from addressbook u");
       Groups groups = new Groups();
-      while (rs.next()) {
+      Users users = new Users();
+ /*     while (rs.next()) {
         groups.add(new GroupData().withId(rs.getInt("group_id")).withName(rs.getString("group_name"))
                 .withHeader(rs.getString("group_header")).withFooter(rs.getString("group_footer")));
       }
-      rs.close();
+      rs.close();*/
+      while (us.next()){
+        users.add(new UserData().withId(us.getInt("id"))
+                .withFirstname(us.getString("firstname")).withLastname(us.getString("lastname")));
+      }
+      us.close();
       st.close();
       conn.close();
 
-      System.out.println(groups);
+  //    System.out.println(groups);
+      System.out.println(users);
 
     } catch (SQLException ex) {
       // handle any errors
