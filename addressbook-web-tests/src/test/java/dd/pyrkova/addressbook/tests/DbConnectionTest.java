0ef92d1c39624a926c8fd607b7a07bc6c65ba4dd
DashaPyr <56103490+DashaPyr@users.noreply.github.com>
2020-03-21 10:02:33 +0300

    Урок 7.1. Драйвер БД. Получение данных из БД

---
+++ addressbook-web-tests/src/test/java/dd/pyrkova/addressbook/tests/DbConnectionTest.java
@@ -0,0 +1,37 @@
+package dd.pyrkova.addressbook.tests;
+
+import dd.pyrkova.addressbook.model.GroupData;
+import dd.pyrkova.addressbook.model.Groups;
+import org.testng.annotations.Test;
+
+import java.sql.*;
+
+public class DbConnectionTest {
+
+  @Test
+  public void testDbConnection() {
+    Connection conn = null;
+
+    try {
+      conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=&serverTimezone=UTC");
+      Statement st = conn.createStatement();
+      ResultSet rs = st.executeQuery("select l.group_id, l.group_name, l.group_header, l.group_footer from group_list l");
+      Groups groups = new Groups();
+      while (rs.next()) {
+        groups.add(new GroupData().withId(rs.getInt("group_id")).withName(rs.getString("group_name"))
+                .withHeader(rs.getString("group_header")).withFooter(rs.getString("group_footer")));
+      }
+      rs.close();
+      st.close();
+      conn.close();
+
+      System.out.println(groups);
+
+    } catch (SQLException ex) {
+      // handle any errors
+      System.out.println("SQLException: " + ex.getMessage());
+      System.out.println("SQLState: " + ex.getSQLState());
+      System.out.println("VendorError: " + ex.getErrorCode());
+    }
+  }
+}
