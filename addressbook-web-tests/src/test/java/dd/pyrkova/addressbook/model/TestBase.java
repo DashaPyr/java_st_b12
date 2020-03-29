c8397d475cc743fa23bd53b2d38b16a5b6dc5cc1
DashaPyr <56103490+DashaPyr@users.noreply.github.com>
2020-03-29 09:54:50 +0300

    переключение браузера

--- addressbook-web-tests/src/test/java/dd/pyrkova/addressbook/model/TestBase.java
+++ addressbook-web-tests/src/test/java/dd/pyrkova/addressbook/model/TestBase.java
@@ -22,7 +22,7 @@ public class TestBase {
   Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   public static final ApplicationManager app
-          = new ApplicationManager(System.getProperty("browser", BrowserType.IE));
+          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite(alwaysRun = true)
   public void setUp() throws Exception {
