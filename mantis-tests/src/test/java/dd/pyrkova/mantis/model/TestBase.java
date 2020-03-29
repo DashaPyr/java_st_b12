4818519801a20745010e63f4b8421267aa4e6f30
DashaPyr <56103490+DashaPyr@users.noreply.github.com>
2020-03-29 19:54:19 +0300

    8.5 FTP TurnOFF captcha

--- mantis-tests/src/test/java/dd/pyrkova/mantis/model/TestBase.java
+++ mantis-tests/src/test/java/dd/pyrkova/mantis/model/TestBase.java
@@ -17,10 +17,12 @@ public class TestBase {
   @BeforeSuite
   public void setUp() throws IOException {
     app.init();
+    app.ftp().upload(new File("src/test/resources/config_inc.php"), "config/config_inc.php", "config/config_inc.php.bak");
   }
 
   @AfterSuite(alwaysRun = true)
-  public void tearDown(){
+  public void tearDown() throws IOException {
+    app.ftp().restore("config/config_inc.php.bak", "config/config_inc.php");
     app.stop();
   }
 }
