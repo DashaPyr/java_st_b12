61707bd005801ebce4db583044b7e067585af190
DashaPyr <56103490+DashaPyr@users.noreply.github.com>
2020-03-29 15:49:20 +0300

    8.3 fix

--- mantis-tests/src/test/java/dd/pyrkova/mantis/model/TestBase.java
+++ mantis-tests/src/test/java/dd/pyrkova/mantis/model/TestBase.java
@@ -6,18 +6,21 @@ import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import java.io.File;
+import java.io.IOException;
+
 public class TestBase {
 
   public static final ApplicationManager app
           = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
-  @BeforeSuite(alwaysRun = true)
-  public void setUp() throws Exception {
+  @BeforeSuite
+  public void setUp() throws IOException {
     app.init();
   }
 
   @AfterSuite(alwaysRun = true)
-  public void tearDown() throws Exception {
+  public void tearDown(){
     app.stop();
   }
 }
