a49321123e55974f5d10695393cd9dee7e5919a6
DashaPyr <56103490+DashaPyr@users.noreply.github.com>
2020-03-29 11:09:07 +0300

    8.2 добавлен новый модуль для тестов Mantis

---
+++ mantis-tests/src/test/java/dd/pyrkova/mantis/model/TestBase.java
@@ -0,0 +1,25 @@
+package dd.pyrkova.mantis.model;
+
+
+import dd.pyrkova.mantis.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class TestBase {
+
+  public static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+  @BeforeSuite(alwaysRun = true)
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown() throws Exception {
+    app.stop();
+  }
+}
+
+
