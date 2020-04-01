19227c972038bc68b3e41cd46278f7738ceeefeb
DashaPyr <56103490+DashaPyr@users.noreply.github.com>
2020-04-01 11:17:10 +0300

    8.7 fixed

--- mantis-tests/src/test/java/dd/pyrkova/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/dd/pyrkova/mantis/tests/RegistrationTests.java
@@ -24,7 +24,7 @@ public class RegistrationTests extends TestBase {
   public void testRegistration() throws IOException, MessagingException {
     long now = System.currentTimeMillis();
     String user = String.format("user%s", now);
-    String email = String.format("user%s@localhost.localdomain", now);
+    String email = String.format("user%s@localhost", now);
     String password = "password";
     app.james().createUser(user, password);
     app.registration().start(user, email);
